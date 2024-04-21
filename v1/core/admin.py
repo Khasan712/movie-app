79af9422a55aa22f6ab3f515f33dded6a571c755
Khasan <info.kamalov@gmail.com>
2024-04-21 19:28:33 +0500

    added banner and mylist

--- v1/core/admin.py
+++ v1/core/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (
-    Cinema, Genre, CadreCinema, Category, Series
+    Cinema, Genre, CadreCinema, Category, Series, Banner, MyList
 )
 
 admin.site.register(Cinema)
@@ -8,3 +8,5 @@ admin.site.register(Genre)
 admin.site.register(CadreCinema)
 admin.site.register(Category)
 admin.site.register(Series)
+admin.site.register(Banner)
+admin.site.register(MyList)
