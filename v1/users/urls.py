3ec90e0e18d3c40f3e97a0d105e874ffa2fb4f24
Khasan <info.kamalov@gmail.com>
2024-05-25 12:35:15 +0500

    User data api

--- v1/users/urls.py
+++ v1/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomerRegisterApiV1, CustomLoginApiV1
+from .views import CustomerRegisterApiV1, CustomLoginApiV1, MeApiV1
 
 
 urlpatterns = [
+    path('me/', MeApiV1.as_view()),
     path('login/', CustomLoginApiV1.as_view()),
     path('customer-register/', CustomerRegisterApiV1.as_view()),
 ]
--- v1/users/views.py
+++ v1/users/views.py
@@ -4,6 +4,22 @@ from .serializers import CustomerRegisterSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
+
+class MeApiV1(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        user = request.user
+        return Response({
+            'id': user.id,
+            'phone': user.phone_number,
+            'first_name': user.first_name,
+            'last_name': user.last_name,
+            'role': user.role,
+        })
 
 
 class CustomLoginApiV1(ObtainAuthToken):
